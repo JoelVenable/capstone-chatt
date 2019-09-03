0beefa568a0fffc445d849abd69f5a2622ebdb1e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-03 09:33:49 -0500

    Initial setup of dotnet 3.0 react app with auth

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone_chatt.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+
+        public string Handle { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+
+        public string ImageUrl { get; set; }
+
+        public string ThumbUrl { get; set; }
+
+        public bool IsOnline { get; set; }
+
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone_chatt.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+
+        public Guid SenderId { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public Guid ParentMessageId { get; set; }
+
+        public List<Message> Thread { get; set; }
+    }
+}
