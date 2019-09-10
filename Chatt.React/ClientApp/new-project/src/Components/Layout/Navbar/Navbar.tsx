dfcdaef9ad74d18ba210869e2e1474998d867d19
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-10 08:55:50 -0500

    Updated with new React project

---
+++ Chatt.React/ClientApp/new-project/src/Components/Layout/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { AppBar } from "@material-ui/core";
+
+const NavBar: React.FC = () => {
+  return <AppBar></AppBar>;
+};
+
+export default NavBar;
--- Chatt.React/ClientApp/src/App.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//it('renders without crashing', () => {
-//  const div = document.createElement('div');
-//  ReactDOM.render(<App />, div);
-//  ReactDOM.unmountComponentAtNode(div);
-//});
-//# sourceMappingURL=App.test.js.map
