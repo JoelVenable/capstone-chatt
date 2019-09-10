75407529308bf61ccc0abf3609066e783dcfb7b5
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-10 08:54:25 -0500

    Updated line endings

--- Chatt.React/Startup.cs
+++ Chatt.React/Startup.cs
@@ -111,7 +111,8 @@ namespace Chatt.React
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+                    //spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
         }
