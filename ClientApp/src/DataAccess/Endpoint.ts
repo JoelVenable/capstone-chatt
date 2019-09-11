7bb474ba631dca22ff84e40e2ab306fa3268ee1e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-11 18:09:37 -0500

    Working on data access layer

--- ClientApp/src/DataAccess/Endpoint.ts
+++ ClientApp/src/DataAccess/Endpoint.ts
@@ -12,29 +12,114 @@ with the following methods:
 }
 */
 
+import decode from "jwt-decode";
+import moment from "moment";
 
-export {}
+const baseURL = `/api`;
+const LS_ITEM = "id_token";
 
-//export class Endpoint<T> {
-//    private const baseURL = 'http://localhost:8088/api'
+const endpoints = {
+  LOGIN: "/api/auth/login",
+  REGISTER: "/api/auth/register"
+};
 
-//    create(url: string): Promise<T> {
-//        return fetch(url, {
-//            method: 'POST',
-//            headers: {
-//                'Content-Type': 'application/json',
-//            },
-//            body: JSON.stringify(obj),
-//        })
-//            .then(checkAndParseResponse)
-//            .catch(catchError);
-//    }
+export class Endpoint<T, R> {
+  //  ${process.env.PUBLIC_URL}
 
-//    constructor(url: string): {
-//        create = obj => 
-//    }
-    
+  private endpointURL: string;
 
+  constructor(extension: string) {
+    this.endpointURL = `${baseURL}/${extension}`;
+  }
+
+  private setToken = (idToken: string): void =>
+    localStorage.setItem(LS_ITEM, idToken);
+  private getToken = () => localStorage.getItem(LS_ITEM);
+  logout = (): void => localStorage.removeItem(LS_ITEM);
+
+  login = async (cred: IUserCredentials): Promise<IActionResult> => {
+    const request = new Request(endpoints.LOGIN, {
+      method: "POST",
+      body: JSON.stringify(cred),
+      headers: this.buildHeaders()
+    });
+
+    const result = await fetch(request)
+      .then(this.checkStatus)
+      .then(response => response.json())
+      .then(res => {
+        this.setToken(res.token);
+        return Promise.resolve(res);
+      });
+    console.log(result);
+
+    return { response: "SUCCESS" };
+  };
+
+  private checkStatus(response: Response): Response {
+    if (response.status >= 200 && response.status < 300) return response;
+    else {
+      throw new Error(response.statusText);
+    }
+  }
+
+  private isTokenExpired = (token: string): boolean => {
+    try {
+      const decoded = decode<IToken>(token);
+      const expDate = moment(decoded.expiration);
+
+      if (expDate.isBefore(Date.now())) return true;
+      else return false;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  private loggedIn = (): boolean => {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  };
+
+  buildHeaders = (): HeadersInit => {
+    let requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set("Content-Type", "application/json");
+    requestHeaders.set("Accept", "application/json");
+
+    if (this.loggedIn()) {
+      requestHeaders.set("credentials", "include");
+      requestHeaders.set("Authorization", `Bearer ${this.getToken()}`);
+    }
+    return requestHeaders;
+  };
+
+  // fetch = (urlArgs: string = "", options: ResponseInit): Promise<R> => {
+  //   const headers = this.buildHeaders(options);
+
+  //   return fetch(`${this.endpointURL}/${urlArgs}`, headers)
+  //     .then(this.checkStatus)
+  //     .then(response => response.json());
+  // };
+
+  // getProfile = () => decode(this.getToken());
+
+  // private endpointUrl: string;
+
+  // constructor(urlExtension: string) {
+  //   this.endpointUrl = `${this.baseURL}/${urlExtension}`;
+  // }
+
+  // post(url: string, obj: T): Promise<R> {
+  //   const urlToSend = `${this.endpointUrl}/${url}`;
+  //   console.log(urlToSend);
+  //   return this.fetch(urlToSend, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(obj)
+  //   }).then(response => response.json());
+  // }
+}
 
 //  this.getAll = params => fetch(url + (params || ''))
 //    .then(checkAndParseResponse)
@@ -43,7 +128,6 @@ export {}
 //  this.getOne = id => fetch(`${url}/${id}`)
 //    .then(checkAndParseResponse)
 //    .catch(catchError);
-
 
 //  this.update = (id, object) => fetch(`${url}/${id}`, {
 //    method: 'PATCH',
@@ -55,7 +139,6 @@ export {}
 //    .then(checkAndParseResponse)
 //    .catch(catchError);
 
-
 //  this.replace = (id, newObject) => fetch(`${url}/${id}`, {
 //    method: 'PUT',
 //    headers: {
@@ -65,7 +148,6 @@ export {}
 //  })
 //    .then(checkAndParseResponse)
 //    .catch(catchError);
-
 
 //  this.delete = id => fetch(`${url}/${id}`, {
 //    method: 'DELETE',
