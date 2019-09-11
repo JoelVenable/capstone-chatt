1b273ca34557a6a22d70d3f84cdbdcdade078539
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-11 18:09:19 -0500

    Updated views

---
+++ ClientApp/src/Views/Homepage/Homepage.tsx
@@ -0,0 +1,21 @@
+﻿import * as React from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+const Homepage: React.FC<RouteComponentProps<any>> = () => {
+  return (
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  );
+};
+
+export default Homepage;
--- ClientApp/src/Views/Login/Login.tsx
+++ ClientApp/src/Views/Login/Login.tsx
@@ -1,87 +1,87 @@
-﻿// import * as React from "react";
-// import { TextField } from "@material-ui/core";
-// import { Link, withRouter } from "react-router-dom";
+﻿import * as React from "react";
+import { TextField } from "@material-ui/core";
+import { Link, withRouter } from "react-router-dom";
 
-export {};
+import validateEmail from "../../Utility/validateEmail";
+import { useAuthContext } from "../../Context/useAuthContext";
+import { AuthPageContainer } from "../../Layout/AuthPageContainer/AuthPageContainer";
+import CenteredButton from "../../Components/CenteredButton/CenteredButton";
+import { routeDefinitions } from "../../Router/routeDefinitions";
+const LoginView = withRouter(({ history }) => {
+  const { actions } = useAuthContext();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [disabled, setDisabled] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
 
-// import validateEmail from "../../Utility/validateEmail";
+  const validateForm = (): boolean =>
+    validateEmail(email) && password.length > 0;
 
-// import { AuthContext } from "../../Context/AuthContext";
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { id, value } = e.currentTarget;
+    if (id === "email") setEmail(value);
+    if (id === "password") setPassword(value);
 
-// const LoginView = withRouter(({ history }) => {
-//   const { actions } = React.useContext(AuthContext);
-//   const [email, setEmail] = React.useState("");
-//   const [password, setPassword] = React.useState("");
-//   const [disabled, setDisabled] = React.useState(true);
-//   const [loading, setLoading] = React.useState(false);
+    const isInvalid = !validateForm();
+    if (disabled !== isInvalid) setDisabled(isInvalid);
+  };
 
-//   const validateForm = (): boolean =>
-//     validateEmail(email) && password.length > 0;
+  const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
+    e.preventDefault();
+    setLoading(true);
+    debugger;
+    const credentials: IUserCredentials = { username: email, password };
+    const { response } = await actions.signIn(credentials);
 
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-//     const { id, value } = e.currentTarget;
-//     if (id === "email") setEmail(value);
-//     if (id === "password") setPassword(value);
+    if (response === "SUCCESS") history.push(routeDefinitions.HOMEPAGE);
 
-//     const isInvalid = !validateForm();
-//     if (disabled !== isInvalid) setDisabled(isInvalid);
-//   };
+    setLoading(false);
+  };
 
-//   const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
-//     e.preventDefault();
-//     setLoading(true);
-//     const credentials: IUserCredentials = { username: email, password };
-//     const result = await actions.signIn(credentials);
+  return (
+    <AuthPageContainer>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          required
+          fullWidth
+          value={email}
+          onChange={handleChange}
+          margin="normal"
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          required
+          fullWidth
+          value={password}
+          onChange={handleChange}
+          margin="normal"
+        />
+        <CenteredButton
+          color="primary"
+          variant="contained"
+          style={{ fontSize: "1.3rem" }}
+          disabled={disabled || loading}
+          type="submit">
+          Sign In
+        </CenteredButton>
+        {/* <CenteredButton
+          style={{ fontSize: "1.3rem" }}
+          component={Link}
+          to="/signup">
+          Register
+        </CenteredButton>
+        <CenteredButton component={Link} to="/recover">
+          Forgot Password?
+        </CenteredButton> */}
+      </form>
+    </AuthPageContainer>
+  );
+});
 
-//     if (result === "SUCCESS") history.push("/");
-
-//     setLoading(false);
-//   };
-
-//   return (
-//     <AuthPageContainer>
-//       <form onSubmit={handleSubmit}>
-//         <TextField
-//           id="email"
-//           label="Email"
-//           variant="outlined"
-//           required
-//           fullWidth
-//           value={email}
-//           onChange={handleChange}
-//           margin="normal"
-//         />
-//         <TextField
-//           id="password"
-//           label="Password"
-//           variant="outlined"
-//           type="password"
-//           required
-//           fullWidth
-//           value={password}
-//           onChange={handleChange}
-//           margin="normal"
-//         />
-//         <CenteredButton
-//           color="primary"
-//           variant="contained"
-//           style={{ fontSize: "1.3rem" }}
-//           disabled={disabled || loading}
-//           type="submit">
-//           Sign In
-//         </CenteredButton>
-//         <CenteredButton
-//           style={{ fontSize: "1.3rem" }}
-//           component={Link}
-//           to="/signup">
-//           Register
-//         </CenteredButton>
-//         <CenteredButton component={Link} to="/recover">
-//           Forgot Password?
-//         </CenteredButton>
-//       </form>
-//     </AuthPageContainer>
-//   );
-// });
-
-// export default LoginView;
+export default LoginView;
