45e93335890ffc26d186c10c90c160de92d2ce4f
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-11 18:10:35 -0500

    Added auth container and moved Authenticated Route component

--- ClientApp/src/ApplicationViews.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import Homepage from './Views/Homepage/Index';
-export const ApplicationViews = () => {
-    return (React.createElement(Route, { path: "/", component: Homepage }));
-};
-//# sourceMappingURL=ApplicationViews.js.map
