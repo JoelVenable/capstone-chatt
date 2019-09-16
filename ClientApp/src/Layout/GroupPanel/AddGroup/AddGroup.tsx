48635b4f25898a04e1ca65c1cc6ed675490b11b2
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-16 12:40:32 -0500

    Successfully adds a group to the database

--- ClientApp/src/Layout/GroupPanel/AddGroup/AddGroup.tsx
+++ ClientApp/src/Layout/GroupPanel/AddGroup/AddGroup.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Dialog,
   Button,
   DialogTitle,
   TextField,
   DialogContent,
-  DialogActions
+  DialogActions,
+  FormGroup,
+  FormControlLabel,
+  Checkbox
 } from "@material-ui/core";
 import { myColors } from "../../../theme";
 import { groupManager } from "../../../DataAccess/groupManager";
@@ -18,16 +21,35 @@ interface Props {
 const AddGroup: React.FC<Props> = ({ update, setUpdate }: Props) => {
   const [isModalOpen, setModalOpen] = useState<boolean>(true);
   const [newGroupName, setNewGroupName] = useState<string>("");
+  const [groupPrivate, setGroupPrivate] = useState<boolean>(false);
+  const [groupProtected, setGroupProtected] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (newGroupName.length > 0) setDisabled(false);
+    else setDisabled(true);
+  }, [newGroupName]);
+
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = e => {
-    setNewGroupName(e.currentTarget.value as string);
+    const { id, value } = e.currentTarget;
+    if (id === "groupName") setNewGroupName(e.currentTarget.value as string);
+    if (id === "groupPrivate") setGroupPrivate(!groupPrivate);
+    if (id === "groupProtected") setGroupProtected(!groupProtected);
   };
 
-  const handleSubmit: React.FormEventHandler = (e) => {
+  const handleSubmit: React.FormEventHandler = async e => {
     e.preventDefault();
-    const {response} = await groupManager.
+    const { response } = await groupManager.post({
+      name: newGroupName,
+      isPrivate: groupPrivate,
+      isProtected: groupProtected
+    });
+    setUpdate(Math.random());
+    console.log(response);
   };
 
   return (
@@ -45,13 +67,44 @@ const AddGroup: React.FC<Props> = ({ update, setUpdate }: Props) => {
               value={newGroupName}
               style={{ backgroundColor: myColors.white }}
               onChange={handleChange}
+              disabled={loading}
+              id="groupName"
               variant="filled"
             />
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={groupPrivate}
+                    color="primary"
+                    disabled={loading}
+                    checked={groupPrivate}
+                    onChange={handleChange}
+                    id="groupPrivate"
+                  />
+                }
+                label="Private Group"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={groupProtected}
+                    color="primary"
+                    checked={groupProtected}
+                    onChange={handleChange}
+                    id="groupProtected"
+                  />
+                }
+                label="Protected Group"
+              />
+            </FormGroup>
           </form>
         </DialogContent>
         <DialogActions>
           <Button onClick={closeModal}>Cancel</Button>
-          <Button onClick={handleSubmit}>Add Group</Button>
+          <Button onClick={handleSubmit} disabled={disabled || loading}>
+            Add Group
+          </Button>
         </DialogActions>
       </Dialog>
     </>
