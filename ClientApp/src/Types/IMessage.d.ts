c9b4d869869de3025494b3edb62083890bcd4019
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-16 14:52:48 -0500

    Updated types

--- ClientApp/src/Types/IMessage.d.ts
+++ ClientApp/src/Types/IMessage.d.ts
@@ -1,1 +1,14 @@
-interface IMessage {}
+interface IMessage {
+  id?: string;
+  dateCreated?: string;
+  senderId: string;
+  sender?: IApplicationUser;
+  groupId: string;
+  group?: IGroup;
+  text: string;
+  thread?: IMessage[];
+  parentMessageId?: string;
+  parentMessage?: IMessage;
+  reactions?: IReaction[];
+  isDeleted?: boolean;
+}
