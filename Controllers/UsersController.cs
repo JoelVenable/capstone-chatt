69a8ddc5fe0e10ad6849d335eb7d55e16b2e23d9
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-09-17 11:11:34 -0500

    Scaffolded users controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chatt.Data;
+using Chatt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager
+            )
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+    }
+}
